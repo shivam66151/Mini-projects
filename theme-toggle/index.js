b1cdaa4ab657034332d63faab14064650e21feb6
Shivam Sharma <sharmashivam66151@gmail.com>
2022-02-04 15:03:33 +0000

    minor changes

--- theme-toggle/index.js
+++ theme-toggle/index.js
@@ -1,6 +1,23 @@
-let darkmode = localStorage.getItem("darkMode")
+let darkMode = localStorage.getItem("darkMode");
 
-function enabledarkmode {
-    document.body.classList.add("darkmode")
-    localStorage.setItem("darkmode", "enabled");
+function enableDarkMode() {
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkMode", "enabled");
 }
+
+function disableDarkMode() {
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkMode", null);
+}
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+// Listeners
+
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+darkModeToggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  darkMode !== "enabled" ? enableDarkMode() : disableDarkMode();
+});
